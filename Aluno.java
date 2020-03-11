b8e28b4a6a5e87c7ced558e313ef3909db9fd144
unknown <gabriel.laranjeira@bravomob.com>
2020-03-10 23:05:40 -0300

    Criação de set's

--- Aluno.java
+++ Aluno.java
@@ -2,4 +2,12 @@ public class Aluno{
    private String nome;
    private int id;
    
+   public void setNome(String nome){
+	   this->nome = nome;
+   }
+   
+   public void setId(int id){
+	   this->id = id;
+   }
+   
 }
